18313edec05810264312542eb57687ce7c7d7610
ulisao <ulisessbaretta@gmail.com>
2023-02-26 20:23:45 -0300

    uptade info

--- src/components/about/Team.js
+++ src/components/about/Team.js
@@ -14,8 +14,8 @@
 */
 const people = [
     {
-      name: 'Lindsay Walton',
-      role: 'Front-end Developer',
+      name: 'Ulises Baretta',
+      role: 'Full-stack Developer / CEO',
       imageUrl:
         'https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80',
       twitterUrl: '#',
@@ -30,7 +30,7 @@ const people = [
         <div className="mx-auto lg:mx-12 max-w-full py-12 px-4 sm:px-6 lg:px-8 lg:py-24">
           <div className="space-y-12">
             <div className="space-y-5 sm:space-y-4 md:max-w-xl lg:max-w-3xl xl:max-w-none">
-              <h2 className="text-3xl font-bold tracking-tight sm:text-4xl">Our Team</h2>
+              <h2 className="text-3xl font-bold tracking-tight sm:text-4xl">Nuestro Equipo</h2>
               <p className="text-xl text-gray-500">
                 Odio nisi, lectus dis nulla. Ultrices maecenas vitae rutrum dolor ultricies donec risus sodales. Tempus
                 quis et.
--- src/containers/pages/Services.jsx
+++ src/containers/pages/Services.jsx
@@ -8,12 +8,12 @@ import { useEffect } from "react"
 import {Helmet}from'react-helmet-async'
 const posts_software = [
     {
-      title: 'Boost your conversion rate',
+      title: 'Eccomerce',
       img: coding_img,
       href: '#',
       category: { name: 'Article', href: '#' },
       description:
-        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto accusantium praesentium eius, ut atque fuga culpa, similique sequi cum eos quis dolorum.',
+        'Creamos tu tienda virtual para que puedas vender tus productos de forma facil y eficiente.',
       date: 'Mar 16, 2020',
       datetime: '2020-03-16',
       imageUrl:
@@ -27,12 +27,12 @@ const posts_software = [
       },
     },
     {
-      title: 'How to use search engine optimization to drive sales',
+      title: 'Landing pages',
       href: '#',
       img: coding_img,
       category: { name: 'Video', href: '#' },
       description:
-        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit facilis asperiores porro quaerat doloribus, eveniet dolore. Adipisci tempora aut inventore optio animi., tempore temporibus quo laudantium.',
+        'Promociona tu propia empresa o producto con una pagina como la que estas visitando',
       date: 'Mar 10, 2020',
       datetime: '2020-03-10',
       imageUrl:
@@ -46,72 +46,12 @@ const posts_software = [
       },
     },
     {
-      title: 'Improve your customer experience',
+      title: 'Paginas personalizadas',
       href: '#',
       img: coding_img,
       category: { name: 'Case Study', href: '#' },
       description:
-        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint harum rerum voluptatem quo recusandae magni placeat saepe molestiae, sed excepturi cumque corporis perferendis hic.',
-      date: 'Feb 12, 2020',
-      datetime: '2020-02-12',
-      imageUrl:
-        'https://images.unsplash.com/photo-1492724441997-5dc865305da7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1679&q=80',
-      readingTime: '11 min',
-      author: {
-        name: 'Daniela Metz',
-        href: '#',
-        imageUrl:
-          'https://images.unsplash.com/photo-1487412720507-e7ab37603c6f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-      },
-    },
-  ]
-  
-const posts_design = [
-    {
-      title: 'Boost your conversion rate',
-      img: coding_img,
-      href: '#',
-      category: { name: 'Article', href: '#' },
-      description:
-        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto accusantium praesentium eius, ut atque fuga culpa, similique sequi cum eos quis dolorum.',
-      date: 'Mar 16, 2020',
-      datetime: '2020-03-16',
-      imageUrl:
-        'https://images.unsplash.com/photo-1496128858413-b36217c2ce36?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1679&q=80',
-      readingTime: '6 min',
-      author: {
-        name: 'Roel Aufderehar',
-        href: '#',
-        imageUrl:
-          'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-      },
-    },
-    {
-      title: 'How to use search engine optimization to drive sales',
-      href: '#',
-      img: coding_img,
-      category: { name: 'Video', href: '#' },
-      description:
-        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit facilis asperiores porro quaerat doloribus, eveniet dolore. Adipisci tempora aut inventore optio animi., tempore temporibus quo laudantium.',
-      date: 'Mar 10, 2020',
-      datetime: '2020-03-10',
-      imageUrl:
-        'https://images.unsplash.com/photo-1547586696-ea22b4d4235d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1679&q=80',
-      readingTime: '4 min',
-      author: {
-        name: 'Brenna Goyette',
-        href: '#',
-        imageUrl:
-          'https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-      },
-    },
-    {
-      title: 'Improve your customer experience',
-      href: '#',
-      img: coding_img,
-      category: { name: 'Case Study', href: '#' },
-      description:
-        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint harum rerum voluptatem quo recusandae magni placeat saepe molestiae, sed excepturi cumque corporis perferendis hic.',
+        'Hacemos realidad tus ideas de la manera que prefieras.',
       date: 'Feb 12, 2020',
       datetime: '2020-02-12',
       imageUrl:
@@ -158,11 +98,10 @@ function Services(){
             <Navbar/>
             <div className="pt-28">
                 <Header/>
-                <div className="py-12 bg-gray-50">
+                <div className="mt-5 py-12 bg-gray-50">
 
                 </div>
-                <ServicesList posts={posts_software} section_title={'Software and Product Development'}/>
-                <ServicesList posts={posts_design} section_title={'Design Services'}/>
+                <ServicesList posts={posts_software} section_title={'Desarrollo de software y producto'}/>
             </div>
                 <Footer/>
         </Layout>
